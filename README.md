594cc5d27e1090da2abf620af831e145ba4d491b
Reimu Neko Miko <nekomiko444@gmail.com>
2017-09-23 16:29:53 +0300

    Prep refactoring before feature extension.

--- README.md
+++ README.md
@@ -1,21 +1,13 @@
 # Natural language statistics tool for Python projects.
 Following instruments is able to calulate some word-metrics for python project.
+
 ### Usage
 Statistics are calculated by PyWordStat class like following:
 ```
-from dclnt.dclnt import PyWordStat
+from dclnt.dclnt import PyWordStatLocal
 django_stat = PyWordStat("lib/python3.5/site-packages/django")
-print(django_stat.get_top_verbs())
+print(django_stat.get_func_sample("VB"))
 ```
-
-### Features
-This tool is able to deliver following information:
-* `get_top_verbs` -- statistics of most common verbs in function names in project
-* `get_top_functions_names` -- statistics of most common function names in project
-* `get_all_words` -- statistics of most common words used in identifiers
-* `get_all_verbs` -- list of all verbs occuring in function names of project
-* `get_all_function_names` -- list of all function names in project
-* `get_all_words` -- list of all words used in identifiers
 
 ### Dependencies
 Python3, `nltk` -- Natural Language Toolkit
--- dclnt/dclnt.py
+++ dclnt/dclnt.py
@@ -24,18 +24,100 @@ def is_name(node):
     return isinstance(node, ast.Name)
 
 
-def is_verb(word):
+# def is_noun(word):
+#    return check_word_ps(word, "NN")
+
+
+# def is_verb(word):
+#    return check_word_ps(word, "VB")
+
+
+def check_word_ps(word, ps=None):
+    '''Checks if word belongs to specified part of speech
+    VB - verb
+    NN - noun'''
     if not word:
         return False
     pos_info = pos_tag([word])
-    return pos_info[0][1] == 'VB'
+    if ps is None:
+        return True
+    return pos_info[0][1] == ps
 
 
 def get_top(_iter, top_size=10):
-        return collections.Counter(_iter).most_common(top_size)
+    return collections.Counter(_iter).most_common(top_size)
 
 
-class PyWordStat:
+class BaseWordStat:
+    '''Common interface for statistics calculation.
+    Assumes _tree_nodes method returns iterator of AST nodes'''
+    def get_name_all(self, local_asg=False):
+        '''Returns generator of all identifiers names in project,
+        optionally only assignments'''
+        def asg_filter(node):
+            return isinstance(node.ctx, ast.Store)
+
+        def always_true(node):
+            return True
+        if local_asg:
+            filter_names = asg_filter
+        else:
+            filter_names = always_true
+        return (node.id for node in self._tree_nodes()
+                if is_name(node) and filter_names(node))
+
+    def extract_words_from_ids(self, ident, ps):
+        '''Extracts words by splitting identifiers
+        and filters part of speech specified by `ps`'''
+        all_names = (f for f in ident
+                     if not is_magic_method(f))
+
+        def split_and_check(name):
+            return [word for word in name.split('_')
+                    if word and check_word_ps(word, ps)]
+        return flat([split_and_check(name) for name in all_names])
+
+    def get_name_sample(self, ps=None, local_asg=False):
+        return self.extract_words_from_ids(self.get_name_all(local_asg), ps)
+
+    def get_func_all(self):
+        return (node.name.lower() for node in self._tree_nodes()
+                if is_func(node) and (not is_magic_method(node.name)))
+
+    def get_func_sample(self, ps=None):
+        '''Returns list of all `ps` part of speech occuring
+        as word in function names of project'''
+        return self.extract_words_from_ids(self.get_func_all(), ps)
+
+#    def get_func_verbs(self):
+#        return self.get_func_sample("VB")
+
+#    def get_func_nouns(self):
+#        return self.get_func_sample("NN")
+
+#    def get_top_func_verbs(self, top_size=10):
+#        '''Returns statistics of most common verbs in function names
+#        in project'''
+#        return get_top(self.get_all_func_ps("VB"), top_size)
+
+    def get_top_functions_names(self, top_size=10):
+        '''Returns statistics of most common function names
+        in project'''
+        return get_top(self.get_all_func(), top_size)
+
+    def get_sample_generic(self, sort, ps=None, param={}):
+        if sort == "func_split":
+            return self.get_func_sample(ps)
+        elif sort == "name_split":
+            return self.get_name_sample(ps, "local" in param)
+        elif sort == "func_whole":
+            return self.get_func_all()
+
+    def get_top_generic(self, sort, ps=None, param={}, top_size=10):
+        return get_top(self.get_sample_generic(sort, ps, param), top_size)
+
+
+class PyWordStatLocal(BaseWordStat):
     '''Calculates statistics for python project
     given as a directory on disk'''
 
@@ -84,54 +166,6 @@ class PyWordStat:
     def _tree_nodes(self):
         return chain(*(ast.walk(t) for t in self._trees()))
 
-    def get_all_names(self):
-        '''Returns list of all identifiers names in project'''
-        return [node.id for node in self._tree_nodes() if is_name(node)]
-
-    def get_all_words(self):
-        '''Returns list of all words occuring in identifiers'''
-        function_names = [f for f in self.get_all_names()
-                          if not is_magic_method(f)]
-
-        def split_snake_case_name_to_words(name):
-            return [n for n in name.split('_') if n]
-        return flat([split_snake_case_name_to_words(function_name)
-                     for function_name in function_names])
-
-    def get_top_words(self, top_size=10):
-        '''Returns statistics of most common words used in identifiers'''
-        return get_top(self.get_all_words(), top_size)
-
-    def get_all_verbs(self):
-        '''Returns list of all verbs occuring in
-        function names of project'''
-        def get_verbs_from_function_name(function_name):
-            return [word for word in function_name.split('_')
-                    if is_verb(word)]
-
-        func_names = [node.name.lower() for node in self._tree_nodes()
-                      if is_func(node) and (not is_magic_method(node.name))]
-        print('functions extracted')
-        verbs = flat([get_verbs_from_function_name(function_name)
-                     for function_name in func_names])
-        return verbs
-
-    def get_top_verbs(self, top_size=10):
-        '''Returns statistics of most common verbs in function names
-        in project'''
-        return get_top(self.get_all_verbs(), top_size)
-
-    def get_all_functions_names(self, top_size=10):
-        '''Returns list of all function names in project'''
-        func = [node.name.lower() for node in self._tree_nodes()
-                if is_func(node) and (not is_magic_method(node.name))]
-        return func
-
-    def get_top_functions_names(self, top_size=10):
-        '''Returns statistics of most common function names
-        in project'''
-        return get_top(self.get_all_functions_names(), top_size)
-
 
 def print_proj_stats():
     '''Prints verbs and funcion names statistics for
@@ -153,17 +187,21 @@ def print_proj_stats():
     ]
     p_verbs = []
     p_func = []
+    p_names = []
     for project in projects:
         path = os.path.join('.', project)
-        proj_stat = PyWordStat(path)
-        p_verbs += proj_stat.get_all_verbs()
-        p_func += proj_stat.get_all_functions_names()
+        proj_stat = PyWordStatLocal(path)
+        p_verbs += proj_stat.get_func_sample("VB")
+        p_func += proj_stat.get_func_all()
+        p_names += proj_stat.get_name_sample(ps="NN", local_asg=True)
 
     top_size = 25
     print('verbs statistics')
     print_word_stat(p_verbs, top_size)
     print('function name statistics')
     print_word_stat(p_func, top_size)
+    print('names statistics')
+    print_word_stat(p_names, top_size)
 
 # Download NLTK package if not installed
 if not data.find('taggers/averaged_perceptron_tagger'):
